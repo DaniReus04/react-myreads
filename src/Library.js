9a618f8d1101ff87cdab0dc2a242d044025b3e15
DaniReus04 <daniel_carvalho_amaroo@outlook.com>
2021-09-22 22:07:59 -0300

    App.js and App.css changed for Library.js and Library.css

---
+++ src/Library.js
@@ -0,0 +1,13 @@
+import "./Library.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <strong>Hello Word!</strong>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
